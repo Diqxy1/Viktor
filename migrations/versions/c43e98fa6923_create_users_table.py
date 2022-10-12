48fd0459d93e4175bc9e839ef525fa6c1a485ae9
Diqxy1 <kusanagitakeru@outlook.com>
2022-10-12 16:09:05 -0300

    refactor: rebuilt migrations to a more robust model

--- migrations/versions/c43e98fa6923_create_users_table.py
+++ migrations/versions/c43e98fa6923_create_users_table.py
@@ -19,11 +19,25 @@ depends_on = None
 def upgrade() -> None:
     op.create_table(
         'users',
-        sa.Column('uuid', sa.String(36), primary_key=True),
-        sa.Column('name', sa.String(50), nullable=False),
-        sa.Column('email', sa.String(60), nullable=False),
-        sa.Column('password', sa.String(30), nullable=False),
-        sa.Column('is_active', sa.Boolean, default=True)
+        sa.Column('uuid', sa.String(36), nullable=False, unique=True, primary_key=True),
+        sa.Column('name', sa.String(255), nullable=False),
+        sa.Column('document', sa.String(22), nullable=False, unique=True),
+        sa.Column('password', sa.String(255), nullable=False),
+
+        sa.Column('email', sa.String(100), nullable=True, unique=True),
+        sa.Column('verified_email', sa.Boolean, default=True),
+
+        sa.Column('phone', sa.String(20), nullable=True, unique=True),
+        sa.Column('verified_phone', sa.Boolean, default=False),
+
+        sa.Column('is_active', sa.Boolean, default=False),
+        sa.Column('code_reference', sa.String(20), nullable=False, unique=True),
+
+        # cli step
+        sa.Column('is_staff', sa.Boolean, default=False),
+
+        sa.Column('created_at', sa.DateTime, nullable=False, server_default=sa.func.now()),
+        sa.Column('updated_at', sa.DateTime, nullable=False, server_default=sa.func.now(), server_onupdate=sa.func.now())
     )
 
 
