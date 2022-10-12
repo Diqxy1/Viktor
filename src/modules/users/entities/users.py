6f8169111dcf9fdd939c0a450ab07abe21594807
Diqxy1 <kusanagitakeru@outlook.com>
2022-10-12 16:15:09 -0300

    refactor: rebuiltentitie to a more robust model

--- src/modules/users/entities/users.py
+++ src/modules/users/entities/users.py
@@ -6,8 +6,21 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    uuid = sa.Column(sa.String, primary_key=True, index=True)
-    email = sa.Column(sa.String, unique=True)
-    password = sa.Column(sa.String)
+    uuid = sa.Column(sa.String, primary_key=True, unique=True, index=True)
     name = sa.Column(sa.String)
-    is_active = sa.Column(sa.Boolean, default=True)
+    document = sa.Column(sa.String, nullable=False)
+    password = sa.Column(sa.String, nullable=False)
+
+    email = sa.Column(sa.String, nullable=True, unique=True)
+    verified_email = sa.Column(sa.Boolean, default=False)
+
+    phone = sa.Column(sa.String, nullable=True, unique=True)
+    verified_phone = sa.Column(sa.Boolean, default=False)
+
+    is_active = sa.Column(sa.Boolean, default=False)
+    code_reference = sa.Column(sa.String, nullable=False, unique=True)
+
+    is_staff = sa.Column(sa.Boolean, default=True)
+
+    created_at = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now())
+    updated_at = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now(), server_onupdate=sa.func.now())
